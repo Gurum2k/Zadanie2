93ce9829f409d514863795bd617a5c312817e8fe
Gurum2k <patryho008@o2.pl>
2023-01-27 15:16:54 +0100

    Other page edited

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Im some other page!
+      pgMultifib
+      Patryk Grzywacz
+      I2S 2.1/2
       <Link to="/">Go back home</Link>
     </div>
   );
